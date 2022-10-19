c78c532180e292885b2a02c420080ba9bbc89455
Jakob Kranawetter <jkranawetter05@gmail.com>
2022-10-19 21:43:41 +0200

    start stop support

--- index.py
+++ index.py
@@ -1,5 +1,6 @@
 import asyncio
 import json
+import math
 
 import aioredis
 
@@ -27,25 +28,68 @@ async def pub_channel(channel, obj: object):
     return await redis.publish(channel, json.dumps(obj))
 
 
+per_symbol_price = {}
+
+
+async def call_forever(function):
+    while True:
+        try:
+            await function()
+        except Exception as e:
+            print("ACHTUNG")
+
+        await asyncio.sleep(2)
+
+
+exchange_1_prev_index = {}
+
+
+async def exchange_1():
+    index_prices = await get_all_index_prices()
+
+    for symbol, price in index_prices.items():
+        per_symbol_price[symbol] = price
+        prev_price = exchange_1_prev_index.get(symbol, 0)
+
+        if price != prev_price:
+            await redis.set(join_args(INDEX, symbol), price)
+            await pub_channel(join_args(INDEX, symbol), {'price': price})
+            exchange_1_prev_index[symbol] = price
+
+
+async def get_exchange_2_index(symbol: str):
+    return 10000  # beispiel 10000
+
+
+exchange_2_prev_index = {}
+
+all_symbols = ['btc', 'eth']
+
+
+async def exchange_request_pro_symbol():
+
+    for symbol in all_symbols:
+        other_price = per_symbol_price.get(symbol)
+
+        price = await get_exchange_2_index(symbol)
+        prev_price = exchange_2_prev_index.get(symbol, 0)
+
+        if price != prev_price:
+            await redis.set(join_args(INDEX, symbol), price)
+            await pub_channel(join_args(INDEX, symbol), {'price': price})
+
+            exchange_2_prev_index[symbol] = price
+
+
 async def main():
     """
     Entry
     """
-    prev_index = {}
 
-    while True:
-        index_prices = await get_all_index_prices()
-
-        for symbol, price in index_prices.items():
-            prev_price = prev_index.get(symbol, 0)
-
-            if price != prev_price:
-                await redis.set(join_args(INDEX, symbol), price)
-                await pub_channel(join_args(INDEX, symbol), {'price': price})
-
-                prev_index[symbol] = price
-
-        await asyncio.sleep(2)
+    await asyncio.gather(
+        call_forever(exchange_1),
+        call_forever(exchange_request_pro_symbol)
+    )
 
 
 if __name__ == "__main__":
